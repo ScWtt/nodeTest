35af3d08c481d984774ca7335c7364746b3be4f3
ScWtt <wtt577474@qq.com>
2019-06-28 14:34:57 +0800

    first node

---
+++ vueclient/src/components/courseEdit.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>课程编辑
+        <div>{{sysId}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            sysId:'',
+        }
+    },
+    created(){
+        this.sysId = this.$route.params.sysId
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ vueclient/src/components/wtt.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>this is wtt</p>
+        <button>这是点击按钮</button>
+        <div class="page">hello</div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.page{
+    width: 100px;
+    height: 200px;
+    background-color: blueviolet;
+}
+</style>
+
+
